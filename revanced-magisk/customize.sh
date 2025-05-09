71d59c5069e209e5690a38e553afea8ea1cc2ec0
Nathan Andrew <nathannxx@proton.me>
2025-05-09 21:14:50 +0700

    le changes

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -181,5 +181,5 @@ if [ "$KSU" ] && [ -d "/data/adb/modules/zygisk-assistant" ]; then
 fi
 
 ui_print "* Done"
-ui_print "  by j-hc (github.com/j-hc)"
+ui_print "  by nathan (github.com/imnathanzero)"
 ui_print " "
